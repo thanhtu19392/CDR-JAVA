d7936643bf2123df5cbb9c8b611287b45b8c6227
thanhtu19392 <thanhtu19392@gmail.com>
2018-01-12 12:57:50 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,98 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
+import java.lang.Math;
 
 
 public class Main {
 
-	public static int nbSim = 100;
+	public static int nbSim = 10000;
 	public static int nbStock = 40;
 	public static ArrayList<Loan> listLoans = new ArrayList<Loan>();
 	public static double randProba;
-	public static double loss = 0;
-	public static double severity = 100;
+	public static double probaVar = 0.95;
+	public static int seuil = (int) ((1-probaVar)*nbSim);
+	//public static double [] randProba;
 	public static double totalLoss[] = new double[nbSim];
-	
+	public static ArrayList<Double> totalLossList = new ArrayList<Double>();
+	public static double sum = 0;
+	public static double sumCarre = 0;
+	public static double volatility;
+	public static double sumES =0;
+	public static double average =0;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i =0; i< nbStock; i++){
-			Loan loan = new Loan(i, 0.25, 0.6, 100000);
+			Loan loan = new Loan(i, 0.25, 0.6, 100000000);
 			listLoans.add(loan);
 		}
-		Portfolio portfolio = new Portfolio(0, listLoans);
+		Portfolio portfolio = new Portfolio(0.25, listLoans);
 		
+		double [][] MatrixCorrelation = new double [nbStock][nbStock];
+		for (int row = 0; row < nbStock; row++) {
+			for (int col = 0; col < nbStock; col++) {
+				if (row == col) {
+					MatrixCorrelation[row][col] = 1d;
+				} else {
+					MatrixCorrelation[row][col] = portfolio.getCorrelation();
+				}
+			}
+		}
+		
+		Cholesky choleskyDecomposition = new Cholesky(MatrixCorrelation);
+		double [][] L = choleskyDecomposition.getL ;
+		
+		Simulator simulator = new Simulator();
 		for (int i = 0; i < nbSim ; i++){
 			Iterator<Loan> loanIterator= portfolio.getLoan().iterator();
 			while(loanIterator.hasNext()){
 				Loan loan = loanIterator.next();
-				Simulator simulator = new Simulator();
 				randProba = simulator.getRandomUniform();
-				if (randProba < loan.getProbaDefault() ){
-					System.out.println("Default of obligor " + i);
+				System.out.println(randProba);
+				//randProba = simulator.generateRandomNumberArray(nbStock);
+				
+				if (randProba <= loan.getProbaDefault() ){
+					//System.out.println("Default of obligor " + i);
 					totalLoss[i] += (1- loan.getRecoveryRate()) * loan.exposure;
 				}
 				else{
-					System.out.println("Not default of obligor " + i);
+					//System.out.println("Not default of obligor " + i);
 				}
 			}
 		}
 		
 		for (int i = 0; i< nbSim ; i++){
-			System.out.println(totalLoss[i]);
+			totalLossList.add(totalLoss[i]);
+			sum += totalLoss[i];
+			sumCarre += Math.pow(totalLoss[i], 2);
+			//System.out.println(totalLoss[i]);
 		}
 		
+		average = sum/nbSim;
 		
+		
+		volatility = Math.sqrt((sumCarre/nbSim)- (sum/(nbSim-1)*(sum/nbSim)));
+		System.out.println("Expected Loss:" + average); 
+		System.out.println("Volatility:" + volatility);
+		
+		
+		Collections.sort(totalLossList);
+		Collections.reverse(totalLossList);
+		
+		for (int i =0; i< nbSim; i++){
+			//System.out.println(totalLossList.get(i));
+		}
+		
+		System.out.println("VAR:"+ totalLossList.get(seuil));
+		for (int i =0;i < seuil; i++){
+			sumES += totalLossList.get(i);
+		}
+		System.out.println("ES:" + sumES/seuil);
 	}
-
+	
+	
+	
+	
 }
--- src/Simulator.java
+++ src/Simulator.java
@@ -13,8 +13,55 @@ public class Simulator {
     
     public double getRandomUniform(){
     	double nextItem = r.nextDouble();
-    	System.out.println(nextItem);
+    	//System.out.println(nextItem);
     	return nextItem;
     }
+    
+    /**
+     * Generate a random number
+     * @return a random number
+     */
+    public static double generateRandomNumber() {
+
+        final Random randomGenerator = new Random();
+        final double u1 = randomGenerator.nextDouble();
+        final double u2 = randomGenerator.nextDouble();
+
+        final double number1 = Math.sqrt( -2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
+        final double number2 = Math.sqrt( -2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2);
+
+
+        return number1;
+    }
+    
+    /**
+     * Generate an array of random numbers
+     *
+     * @param size thr half size of numbers
+     * @return a size * 2 array of random numbers
+     */
+    public static double[] generateRandomNumberArray(final int size) {
+        final double[] numbers = new double[size];
+        final Random randomGenerator = new Random();
+
+        int numberIdx = 0;
+        for (int idx = 0; idx < Math.ceil(size/2.0); idx++) {
+            final double u1 = randomGenerator.nextDouble();
+            final double u2 = randomGenerator.nextDouble();
+            final double number1 = Math.sqrt( -2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2);
+            final double number2 = Math.sqrt( -2 * Math.log(u1)) * Math.sin(2 * Math.PI * u2);
+
+            numbers[numberIdx++] = number1;
+            if (numberIdx < numbers.length) {
+                numbers[numberIdx++] = number2;
+            }
+        }
+
+        return numbers;
+    }
+    
+    
+    
+    
 
 }
